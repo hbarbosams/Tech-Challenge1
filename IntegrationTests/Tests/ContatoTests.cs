354c03b1dcea55334701397d30b7ecdae0d1c4b2
Hugo Barbosa <hugo.barbosa@fmd.ag>
2024-07-23 17:44:41 -0300

    test(contatoTests): adiciona novos testes de getbyid e update de contato

--- IntegrationTests/Tests/ContatoTests.cs
+++ IntegrationTests/Tests/ContatoTests.cs
@@ -43,4 +43,73 @@ public class ContatoTests : BaseIntegrationTest
         contatoCriado.Telefone.Should().Be(contato.Telefone);
         contatoCriado.FoneDddId.Should().Be(contato.FoneDddId);
     }
+    
+    
+    [Fact]
+    public async Task GetById_ShouldReturnContact()
+    {
+        // Arrange
+        var contato = new Contato
+        {
+            Nome = "Hugo Barbosa de Melo Santos",
+            Email = "hbarbosams@gmail.com",
+            Telefone = "997439808",
+            FoneDddId = 3
+        };
+        DbContext.Contato.Add(contato);
+        await DbContext.SaveChangesAsync();
+
+        // Act
+        var httpRequest = await HttpClient.GetAsync($"/api/contato/{contato.Id}");
+        
+
+        // Assert
+        var requestContentString = await httpRequest.Content.ReadAsStringAsync();
+        RespostaPadrao respostaPadrao = JsonConvert.DeserializeObject<RespostaPadrao>(requestContentString)!;
+        Contato contatoCriado = JsonConvert.DeserializeObject<Contato>(JsonConvert.SerializeObject(respostaPadrao.Resposta))!;
+
+        
+        Assert.True(httpRequest.IsSuccessStatusCode);
+        Assert.NotNull(contatoCriado);
+        contatoCriado.Nome.Should().Be(contato.Nome);
+        contatoCriado.Email.Should().Be(contato.Email);
+        contatoCriado.Telefone.Should().Be(contato.Telefone);
+        contatoCriado.FoneDddId.Should().Be(contato.FoneDddId);
+    }
+    
+    [Fact]
+    public async Task Update_ShouldUpdateContact()
+    {
+        // Arrange
+        var contato = new Contato
+        {
+            Nome = "Hugo Barbosa de Melo Santos",
+            Email = "hbarbosams@gmail.com",
+            Telefone = "997439808",
+            FoneDddId = 3
+        };
+        DbContext.Contato.Add(contato);
+        await DbContext.SaveChangesAsync();
+
+        contato.Email = "atualizar@teste.com";
+        contato.Telefone = "123456789";
+        var contatoContent = new StringContent(JsonConvert.SerializeObject(contato), Encoding.UTF8, "application/json");
+
+        // Act
+        var httpRequest = await HttpClient.PutAsync("/api/contato", contatoContent);
+        
+
+        // Assert
+        var requestContentString = await httpRequest.Content.ReadAsStringAsync();
+        RespostaPadrao respostaPadrao = JsonConvert.DeserializeObject<RespostaPadrao>(requestContentString)!;
+        Contato contatoCriado = JsonConvert.DeserializeObject<Contato>(JsonConvert.SerializeObject(respostaPadrao.Resposta))!;
+
+        
+        Assert.True(httpRequest.IsSuccessStatusCode);
+        Assert.NotNull(contatoCriado);
+        contatoCriado.Nome.Should().Be(contato.Nome);
+        contatoCriado.Email.Should().Be(contato.Email);
+        contatoCriado.Telefone.Should().Be(contato.Telefone);
+        contatoCriado.FoneDddId.Should().Be(contato.FoneDddId);
+    }
 }
